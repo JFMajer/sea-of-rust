3fdd99f6ae939957d46d5c33f83920bc058d66c6
JFMajer <jakubmajer@live.com>
2023-07-21 12:15:44 +0200

    file with notes

--- rechor/src/main.rs
+++ rechor/src/main.rs
@@ -1,8 +1,23 @@
 use clap::{App, Arg};
 
 fn main() {
-    let _matches = App::new("rechor")
-      .version("0.1.0")
-      .about("Echo in rust")
-      .get_matches();
+    let matches = App::new("rechor")
+        .version("0.1.0")
+        .about("Echo in rust")
+        .arg(
+            Arg::with_name("text")
+                .value_name("TEXT")
+                .help("Input text")
+                .required(true)
+                .min_values(1),
+        )
+        .arg(
+            Arg::with_name("omit_newline")
+                .short('n')
+                .help("Don't print newline")
+                .takes_value(false),
+        )
+        .get_matches();
+
+    println!("{:#?}", matches);
 }
