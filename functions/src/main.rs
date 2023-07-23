792a4f88e67d74d292442395e12c8ffb62bfe1c1
JFMajer <jakubmajer@live.com>
2023-07-23 14:19:26 +0200

    function parameters

--- functions/src/main.rs
+++ functions/src/main.rs
@@ -1,6 +1,9 @@
 fn main() {
     hello();
-    print_this("Hello, world!");
+    print_this("Hello, world!!!");
+    // print_this(); // won't compile
+
+    println!("sum(1, 2) = {}", sum(1, 2));
 }
 
 fn hello() {
@@ -9,4 +12,8 @@ fn hello() {
 
 fn print_this(s: &str) {
     println!("{}", s);
+}
+
+fn sum(x: i32, y: i32) -> i32 {
+    x + y
 }
