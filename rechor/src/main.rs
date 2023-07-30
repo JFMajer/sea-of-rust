9939c2fda83f1edaacd1316863640bc52d4f693a
JFMajer <jakubmajer@live.com>
2023-07-30 14:56:41 +0200

    prints frog face with -f flag

--- rechor/src/main.rs
+++ rechor/src/main.rs
@@ -13,11 +13,39 @@ fn main() {
         )
         .arg(
             Arg::with_name("omit_newline")
-                .short('n')
+                .short("n")
                 .help("Don't print newline")
                 .takes_value(false),
         )
+        .arg(
+            Arg::with_name("prints_a_frog")
+                .short("f")
+                .help("Prints a frog")
+        )
         .get_matches();
 
-    println!("{:#?}", matches);
+   // println!("{:#?}", matches);
+
+   let text = matches.values_of_lossy("text").unwrap();
+   let omit_newline = matches.is_present("omit_newline");
+   let frog = matches.is_present("prints_a_frog");
+   let frog_emoji = '\u{1F438}';
+   let ending = if omit_newline { "" } else { "\n" };
+   
+    // construct the output string
+    let mut output = String::new();
+
+    if frog {
+        output += &text.join(" ");
+        output += &format!(" {}", frog_emoji);
+        output += ending;
+    } else {
+        output += &text.join(" ");
+        output += ending;
+    }
+
+
+    print!("{}", output);
+   
+
 }
