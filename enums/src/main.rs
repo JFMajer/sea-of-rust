864889312f5c3e54cdcf7f1e0ad3547ca0249f93
JFMajer <jakubmajer@live.com>
2023-08-03 19:30:17 +0200

    more enums

--- enums/src/main.rs
+++ enums/src/main.rs
@@ -35,9 +35,41 @@ enum Pet {
 impl Pet {
     fn info(&self) {
         match self {
-            Pet::Dog(d) => println!("Dog: {} is a {} year old {}. Genus is {}.", d.name, d.age, d.breed, d.genus),
-            Pet::Cat(c) => println!("Cat: {} is a {} year old {}. Genus is {}.", c.name, c.age, c.breed, c.genus),
-            Pet::Bird(b) => println!("Bird: {} is a {} year old {}. Genus is {}. Lifespan is {} years.", b.name, b.age, b.breed, b.genus, b.lifespan),
+            Pet::Dog(d) => println!(
+                "Dog: {} is a {} year old {}. Genus is {}.",
+                d.name, d.age, d.breed, d.genus
+            ),
+            Pet::Cat(c) => println!(
+                "Cat: {} is a {} year old {}. Genus is {}.",
+                c.name, c.age, c.breed, c.genus
+            ),
+            Pet::Bird(b) => println!(
+                "Bird: {} is a {} year old {}. Genus is {}. Lifespan is {} years.",
+                b.name, b.age, b.breed, b.genus, b.lifespan
+            ),
+        }
+    }
+}
+
+enum Coin {
+    Penny,
+    Nickel,
+    Dime,
+    Quarter,
+}
+
+fn value_in_cents(c: Coin) -> u8 {
+    match c {
+        Coin::Penny => {
+            println!("Lucky penny!");
+            1
+        }
+        Coin::Nickel => 5,
+        Coin::Dime => 10,
+        // 25
+        Coin::Quarter => {
+            println!("Quarter!");
+            25
         }
     }
 }
@@ -75,10 +107,26 @@ fn main() {
     my_cat.info();
     my_bird.info();
 
+    let coin1 = Coin::Penny;
+    let coin2 = Coin::Quarter;
+
+    println!("Coin 1 is worth {} cents.", value_in_cents(coin1));
+    println!("Coin 2 is worth {} cents.", value_in_cents(coin2));
+
     let some_number: Option<i32> = Some(5);
     let another_number: Option<i16> = Some(6);
     let some_char = Some('a');
     let absent_number: Option<i32> = None;
     // won't compile, different types
     // let sum = some_number + another_number;
+    let five = Some(5);
+    let six = plus_one(five);
+    println!("Value of variable six is {:?}", six);
+}
+
+fn plus_one(x: Option<i32>) -> Option<i32> {
+    match x {
+        None => None,
+        Some(i) => Some(i + 1)
+    }
 }
